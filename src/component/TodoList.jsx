a69c75e15537bf42b43b58b3929f5205f7b1c39b
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-03-21 21:49:34 +0530

    updated additional css

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -6,11 +6,33 @@ export const TodoList = ({ data }) => {
     <div className={style.container}>
       <div className={style.top}>
         <h1>EMPLOYEE LIST</h1>
-        <button>Show All Department </button>
-        <button>Show Marketing</button>
-        <button>Show HR</button>
-        <button>Show IT</button>
-        <button>Show Finance</button>
+        <div className={style.category}>
+          <button onClick={()=>{
+            {data.map((item) => {
+              return (
+                <>
+                  <div className={style.element} key={item.id}>
+                    <ul>
+                      <li>Name: {item.name}</li>
+                      <li>Gender : {item.gender}</li>
+                      <li>Role : {item.role}</li>
+                      <li>Department :{item.department}</li>
+                      <li>Salary :{item.salary}</li>
+                    </ul>
+                  </div>
+                </>
+              );
+            })}
+          }}>Show All Department </button>
+          <button>Show Marketing</button>
+          <button>Show HR</button>
+          <button>Show IT</button>
+          <button>Show Finance</button>
+        </div>
+        <div className={style.sort}>
+          <button>Sort By Salary Ascending</button>
+          <button>Sort By Salary Descending</button>
+        </div>
 
         {data.map((item) => {
           return (
